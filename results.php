daa0fabd12d5130977d7f0b3fc2d43d1556a307b
gferra02 <gferra02@mail.bbk.ac.uk>
2018-04-05 17:14:19 +0100

    Looping over properties for countries

--- results.php
+++ results.php
@@ -34,18 +34,18 @@
 
                     if ($property_count > 0 && $property_count < 6) {
 
-                        print "<table><thead><tr><th>Country</th>";
+                        // print "<table><thead><tr><th>Country</th>";
+
+                        // foreach ($_GET['property'] as $property_selected) {
+                        //     print "<th>$property_selected</th>";
+                        // }
+
+                        // print "</tr></thead><tbody>";
 
                         foreach ($_GET['property'] as $property_selected) {
-                            print "<th>$property_selected</th>";
-                        }
-
-                        print "</tr></thead><tbody>";
-
-                        foreach ($_GET['property'] as $property_selected) {
-                            print "<tr>";
+                            // print "<tr>";
                             foreach ($_GET['country'] as $country_selected) {
-                                print "<td>" . $country_selected . "</td>";
+                                print "<h3>" . $country_selected . "</h3>";
                                 // Path to json levels
                                 switch ($property_selected) {
                                   // Land boundaries, Coastline, Elevation
@@ -85,20 +85,44 @@
                                         // Source: https://jonsuh.com/blog/convert-loop-through-json-php-javascript-arrays-objects/
                                         // echo '<p>Property selected: ' . $property_selected . '</p>';
                                         foreach ($result[$path_to_data[j]][$property_selected] as $key => $value) {
-                                          echo '<strong>' . $key . '</strong>: ' . $value['text'] . '<br>';
+                                          echo '<p><strong>' . $key . '</strong>: ' . $value['text'] . '</p>';
                                         }
                                         break;
                                     case "Germany":
-                                        $url[i] = $base_url . "gm.json?";
-                                        echo " URL: " . $url[i];
+                                        $url[i] = $base_url . "gm.json";
+                                        $string = file_get_contents($url[i]);
+                                        # Read the JSON output into an associative array
+                                        $result  = json_decode($string, true);
+
+                                        // Source: https://jonsuh.com/blog/convert-loop-through-json-php-javascript-arrays-objects/
+                                        // echo '<p>Property selected: ' . $property_selected . '</p>';
+                                        foreach ($result[$path_to_data[j]][$property_selected] as $key => $value) {
+                                          echo '<p><strong>' . $key . '</strong>: ' . $value['text'] . '</p>';
+                                        }
                                         break;
                                     case "Italy":
-                                        $url[i] = $base_url . "it.json?";
-                                        echo " URL: " . $url[i];
+                                        $url[i] = $base_url . "it.json";
+                                        $string = file_get_contents($url[i]);
+                                        # Read the JSON output into an associative array
+                                        $result  = json_decode($string, true);
+
+                                        // Source: https://jonsuh.com/blog/convert-loop-through-json-php-javascript-arrays-objects/
+                                        // echo '<p>Property selected: ' . $property_selected . '</p>';
+                                        foreach ($result[$path_to_data[j]][$property_selected] as $key => $value) {
+                                          echo '<p><strong>' . $key . '</strong>: ' . $value['text'] . '</p>';
+                                        }
                                         break;
                                     case "UK":
-                                        $url[i] = $base_url . "uk.json?";
-                                        echo " URL: " . $url[i];
+                                        $url[i] = $base_url . "uk.json";
+                                        $string = file_get_contents($url[i]);
+                                        # Read the JSON output into an associative array
+                                        $result  = json_decode($string, true);
+
+                                        // Source: https://jonsuh.com/blog/convert-loop-through-json-php-javascript-arrays-objects/
+                                        // echo '<p>Property selected: ' . $property_selected . '</p>';
+                                        foreach ($result[$path_to_data[j]][$property_selected] as $key => $value) {
+                                          echo '<p><strong>' . $key . '</strong>: ' . $value['text'] . '</p>';
+                                        }
                                         break;
                                 }
                             }
